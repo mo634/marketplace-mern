09600c1a93431a97b8e136e44029b32878cd9ba0
MohamedMostafa <mido221gyefue@gmail.com>
2023-10-30 20:16:12 +0200

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,25 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+dotenv.config();
 
-dotenv.config()
+const app = express();
 
-const app = express()
-
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log("connected successfully to DB")
-}).catch((err) => {
-    console.log("error while connect to db" , err)
+//connect dataBase
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("connected successfully to DB");
 })
+.catch((err) => {
+    console.log("error while connect to db", err);
+});
 
-//connect dataBase 
+app.listen(3000, () => {
+    console.log("runnign serever ");
+});
 
-app.listen(3000,() => {
-    console.log("runnign serever ")
-})
+// create user route
+
+app.use("/api/user", userRouter);
